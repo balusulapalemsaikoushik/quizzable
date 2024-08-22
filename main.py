9961f86ca3207789d8c23b87286502e440ad1d43
Sai Koushik Balusulapalem <sbalusulapalem6629@sai-balusulapalem-grade11-608905.lan>
2024-08-22 16:23:22 -0500

    add ability to answer with term, definition, or both

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from copy import deepcopy
 import random
 # import json
 
-def get_frq(terms, length=10, **kwargs):
+def get_frq(terms, length=10, reverse=False, **kwargs):
     frq = {}
-    terms_copy = deepcopy(terms)
+    if reverse:
+        terms_copy = {}
+        for term in terms:
+            terms_copy[terms[term]] = term
+    else:
+        terms_copy = deepcopy(terms)
     for i in range(length):
         possible_terms = list(terms_copy.keys())
         term = random.choice(possible_terms)
@@ -12,9 +17,14 @@ def get_frq(terms, length=10, **kwargs):
         del terms_copy[term]
     return frq
 
-def get_mcq(terms, length=10, n_options=4, **kwargs):
+def get_mcq(terms, length=10, n_options=4, reverse=False, **kwargs):
     mcq = {}
-    terms_copy = deepcopy(terms)
+    if reverse:
+        terms_copy = {}
+        for term in terms:
+            terms_copy[terms[term]] = term
+    else:
+        terms_copy = deepcopy(terms)
     for i in range(length):
         possible_terms = list(terms_copy.keys())
         term = random.choice(possible_terms)
@@ -28,9 +38,14 @@ def get_mcq(terms, length=10, n_options=4, **kwargs):
         mcq[term] = {"_type": "mcq", "options": dict(options)}
     return mcq
 
-def get_true_false(terms, length=10, **kwargs):
+def get_true_false(terms, length=10, reverse=False, **kwargs):
     true_false = {}
-    terms_copy = deepcopy(terms)
+    if reverse:
+        terms_copy = {}
+        for term in terms:
+            terms_copy[terms[term]] = term
+    else:
+        terms_copy = deepcopy(terms)
     for i in range(length):
         possible_terms = list(terms_copy.keys())
         term = random.choice(possible_terms)
@@ -43,7 +58,7 @@ def get_true_false(terms, length=10, **kwargs):
         true_false[term] = {"_type": "tf", "def": definition, "answer": answer}
     return true_false
 
-def get_quiz(terms, types=["mcq", "frq", "tf"], length=10, n_options=4):
+def get_quiz(terms, types=["mcq", "frq", "tf"], length=10, answer_with="def", n_options=4):
     quiz_types = {
         "mcq": get_mcq,
         "frq": get_frq,
@@ -52,7 +67,12 @@ def get_quiz(terms, types=["mcq", "frq", "tf"], length=10, n_options=4):
     quiz = {}
     for i in range(length):
         get_question = quiz_types[random.choice(types)]
-        question = get_question(terms, length=1, n_options=n_options)
+        reverse = False
+        if answer_with == "both":
+            reverse = random.random() < 0.5
+        elif answer_with == "term":
+            reverse = True
+        question = get_question(terms, length=1, reverse=reverse, n_options=n_options)
         quiz.update(question)
     return quiz
 
@@ -60,7 +80,7 @@ if __name__ == "__main__":
     # with open("literature.json") as terms_file:
     #     terms = json.loads(terms_file.read())
     
-    # questions = get_quiz(terms)
+    # questions = get_quiz(terms, answer_with="both")
     # with open("questions.json", "w") as questions_file:
     #     questions_file.write(json.dumps(questions))
 
